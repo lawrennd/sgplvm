ec7a521c583d14cb4ce04d6bd57de19a009a891e
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2013-11-24 20:13:32 +0000

    General update commit

--- matlab/sgplvmToolboxes.m
+++ matlab/sgplvmToolboxes.m
@@ -1,13 +1,5 @@
 % SGPLVMTOOLBOXES Load in the relevant toolboxes for sgplvm.
 
 importLatest('netlab');
-importLatest('prior');
-importLatest('optimi');
-importLatest('datasets');
-importLatest('ndlutil');
-importLatest('mocap');
-importLatest('mltools');
-importLatest('kern');
-importLatest('gp');
-importLatest('fgplvm');
 importLatest('ncca');
+importLatest('GPmat');
